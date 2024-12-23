ba6b7f4bab60e3115d44ccbd78de65012edd460d
abhaypsxd <theabhay626@gmail.com>
2024-12-23 21:54:58 +0530

    added button controls to play on console.

--- rps/app.js
+++ rps/app.js
@@ -9,56 +9,64 @@ function getComputerChoice() {
     return choices[getRandomInt(3)];
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock Paper Scissors!");
-    return choice;
-}
-
-
-function playGame(nums) {
-    
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function playRound() {
-        c_choice = getComputerChoice().toLowerCase();
-        h_choice = getHumanChoice().toLowerCase();
-        if(c_choice=="rock"){
-            if(h_choice=="scissors") computerScore++;
-            else if(h_choice =="paper") humanScore++;
-        }
-        else if(c_choice=="scissors"){
-            if(h_choice=="paper")computerScore++;
-            else if(h_choice=="rock")humanScore++;
-        }
-        else if (c_choice=="paper"){
-            if(h_choice=="rock")computerScore++;
-            else if(h_choice=="scissor")humanScore++;
-        }
-        else{
-            console.log("Invalid entry!")
-        }
+function playRound(choice) {
+    let c_choice = getComputerChoice().toLowerCase();
+    let h_choice = choice;
+    let winner="draw";
+    if(c_choice=="rock"){
+        if(h_choice=="scissors") winner = "computer";
+        else if(h_choice =="paper") winner = "human";
     }
-    for(let i = 0; i<nums; i++)playRound();
-    return [humanScore, computerScore];
+    else if(c_choice=="scissors"){
+        if(h_choice=="paper")winner = "computer";
+        else if(h_choice=="rock")winner = "human";
+    }
+    else if (c_choice=="paper"){
+        if(h_choice=="rock")winner = "computer";
+        else if(h_choice=="scissor")winner = "human";
+    }
+    else{
+        console.log("Invalid entry!")
+    }
+    console.log(winner);
+    return winner;
 }
 
+let computer_score = 0;
+let human_score = 0;
 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+
+
+rock.addEventListener("click",function (){
+    playRound("rock");
+});
+paper.addEventListener("click",function (){
+    playRound("paper");
+});
+scissor.addEventListener("click",function (){
+    playRound("scissor");
+});
+
+if(human_score>computer_score);
 
 // console.log(getComputerChoice());
 // console.log(getHumanChoice());
-let rounds = prompt("Enter the number of rounds: ")
-let scores = playGame(rounds);
-if(scores[0]>scores[1]){
-    console.log("YOU WIN!");
-    console.log("Scores: ","Your Score: ",scores[0]," Bot Score: ",scores[1]);
-}
-else if(scores[0]<scores[1]){
-    console.log("BOT WINS!");
-    console.log("Scores: ","Your Score: ",scores[0]," Bot Score: ",scores[1]);
-}
-else{
-    console.log("IT'S A DRAW!")
-    console.log("Scores: ","Your Score: ",scores[0]," Bot Score: ",scores[1]);
+// let rounds = prompt("Enter the number of rounds: ")
 
-}
+// let scores = [];
+// if(scores[0]>scores[1]){
+//     console.log("YOU WIN!");
+//     console.log("Scores: ","Your Score: ",scores[0]," Bot Score: ",scores[1]);
+// }
+// else if(scores[0]<scores[1]){
+//     console.log("BOT WINS!");
+//     console.log("Scores: ","Your Score: ",scores[0]," Bot Score: ",scores[1]);
+// }
+// else{
+//     console.log("IT'S A DRAW!")
+//     console.log("Scores: ","Your Score: ",scores[0]," Bot Score: ",scores[1]);
+
+// }
